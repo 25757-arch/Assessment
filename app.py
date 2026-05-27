2f421f46aa923bc2b308c962f9bfdba9531ab16d
25757-arch <25757@burnside.school.nz>
2026-05-27 12:43:24 +1200

    db.close

--- app.py
+++ app.py
@@ -22,3 +22,4 @@ def print_all_gpu():
         #print(gpu) #For the second test, the data is displayed in columns, separated by commas, with parentheses at the beginning and end.
         #print(f"{fighter[1]}{fighter[2]}{fighter[3]}{fighter[4]}{fighter[5]}") #For the third test, the data is displayed in columns, no symbols separate, no parentheses.
         print(f"{gpu[1]:<15}{gpu[2]:<15}{gpu[3]:<10}{gpu[4]:<10}{gpu[5]:<10}") #For the fourth test, I reserved some blank spaces to ensure that the data in each column can be aligned.
+        db.close()
