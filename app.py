e1d240a112199368c62f98b557bb20620fb606b1
25757-arch <25757@burnside.school.nz>
2026-05-27 12:37:30 +1200

    For the fourth test, I reserved some blank spaces to ensure that the data in each column can be aligned.

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ def print_all_gpu():
     # loop through all the results
     for gpu in results:
         #print(gpu) #For the second test, the data is displayed in columns, separated by commas, with parentheses at the beginning and end.
-        print(f"{fighter[1]}{fighter[2]}{fighter[3]}{fighter[4]}{fighter[5]}") #For the third test, the data is displayed in columns, no symbols separate, no parentheses.
+        #print(f"{fighter[1]}{fighter[2]}{fighter[3]}{fighter[4]}{fighter[5]}") #For the third test, the data is displayed in columns, no symbols separate, no parentheses.
+        print(f"{gpu[1]:<15}{gpu[2]:<15}{gpu[3]:<6}{gpu[4]:<6}{gpu[5]:<6}") #For the fourth test, I reserved some blank spaces to ensure that the data in each column can be aligned.
