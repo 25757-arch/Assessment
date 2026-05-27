6edaba6e37b844e0ee3d68df1427e08a4fb5a7ce
25757-arch <25757@burnside.school.nz>
2026-05-27 13:15:12 +1200

    Define the print_all_gpu_by_vram. Allow to print the gpu sorted by vram, which is similar to the print_all_gpu.

--- app.py
+++ app.py
@@ -22,5 +22,18 @@ def print_all_gpu():
         #print(gpu) #For the second test, the data is displayed in columns, separated by commas, with parentheses at the beginning and end.
         #print(f"{fighter[1]}{fighter[2]}{fighter[3]}{fighter[4]}{fighter[5]}") #For the third test, the data is displayed in columns, no symbols separate, no parentheses.
         print(f"{gpu[1]:<15}{gpu[2]:<15}{gpu[3]:<10}{gpu[4]:<10}{gpu[5]:<10}") #For the fourth test, I reserved some blank spaces to ensure that the data in each column can be aligned.
-    db.close()
-print_all_gpu()
+    db.close() #loop finished here
+
+#print all the gpu sorted by vram
+def print_all_gpu_by_vram():
+    db = sqlite3.connect(DATABASE) #connect to sqlite3 database
+    cursor = db.cursor() #build a cursor then have the access to operate the data
+    #sql statement
+    sql = "SELECT * from gpu ORDER by vram DESC;" #set an sql query
+    cursor.execute(sql) #execute sql that was just created
+    results = cursor.fetchall() #fetch all results
+    #print format
+    print("brand          model          vram      speed     price     ") #add title on the first line
+    for gpu in results:  
+        print(f"{gpu[1]:<15}{gpu[2]:<15}{gpu[3]:<10}{gpu[4]:<10}{gpu[5]:<10}") 
+    db.close() #loop finished here
