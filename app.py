660b33d7f6be0f958c4f473c3b38a99c37b37366
25757-arch <25757@burnside.school.nz>
2026-05-27 12:41:43 +1200

    add title on the first line

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ def print_all_gpu():
     cursor.execute(sql) #execute sql that was just created
     results = cursor.fetchall() #fetch all results
     #print format
+    print("brand          model          vram      speed     price     ") #add title on the first line
     #print (results) #For the first test, the data was shown in a continuous style disorderly
     # loop through all the results
     for gpu in results:
         #print(gpu) #For the second test, the data is displayed in columns, separated by commas, with parentheses at the beginning and end.
         #print(f"{fighter[1]}{fighter[2]}{fighter[3]}{fighter[4]}{fighter[5]}") #For the third test, the data is displayed in columns, no symbols separate, no parentheses.
-        print(f"{gpu[1]:<15}{gpu[2]:<15}{gpu[3]:<6}{gpu[4]:<6}{gpu[5]:<6}") #For the fourth test, I reserved some blank spaces to ensure that the data in each column can be aligned.
+        print(f"{gpu[1]:<15}{gpu[2]:<15}{gpu[3]:<10}{gpu[4]:<10}{gpu[5]:<10}") #For the fourth test, I reserved some blank spaces to ensure that the data in each column can be aligned.
